4dc4a6700b52ae995f28a16209a627e37d0b8451
Tom Coleman <tom@thesnail.org>
2013-04-14 22:42:45 +1000

    Seems like a better approach.

--- filepicker-load.js
+++ filepicker-load.js
@@ -1,21 +1,27 @@
 //Functions to run after the script tag has loaded
-var filepickerLoadCallback = function(){
-    //filepicker.setKey("YOUR_API_KEY_HERE");
-};
-
-//If the script doesn't load
-var filepickerErrorCallback = function(error){
-    if(typeof console != undefined) {
-        console.log(error);
+Filepicker = {
+  _loaded: false,
+  _loadCallback: function() {},
+  _onload: function() {
+    Filepicker._loaded = true;
+    Filepicker._loadCallback();
+  },
+  load: function(fn) {
+    if (Filepicker._loaded)  {
+      fn();
+    } else {
+      Filepicker._loadCallback = fn;
     }
+  }
+  // XXX: error callback
 };
 
 //Generate a script tag
 var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = '//api.filepicker.io/v1/filepicker.js';
-script.onload = filepickerLoadCallback;
-script.onerror = filepickerErrorCallback;
+script.onload = Filepicker._onload;
+// script.onerror = Filepicker.error;
 
 //Load the script tag
 var head = document.getElementsByTagName('head')[0];
